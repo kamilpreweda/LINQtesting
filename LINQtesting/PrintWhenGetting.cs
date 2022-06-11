621a0e5db6aeb42e6e1af6c24f9a964ffd537f71
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-11 18:20:25 +0200

    Added PrintWhenGetting class and created linq query

---
+++ LINQtesting/PrintWhenGetting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtesting
+{
+    internal class PrintWhenGetting
+    {
+        private int insantceNumber;
+        public int InsantceNumber
+        {
+            set { insantceNumber = value; }
+            get
+            {
+                Console.WriteLine($"Pobieranie nr {insantceNumber}");
+                return insantceNumber;
+            }
+        }
+    }
+}
--- LINQtesting/Program.cs
+++ LINQtesting/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var listOfObjects = new List<PrintWhenGetting>();
+            for (int i = 0; i < 5; i++)
+            {
+                listOfObjects.Add(new PrintWhenGetting() { InsantceNumber = i });
+            }
+
+            Console.WriteLine("Przygotowywanie kwerendy");
+            var result =
+                from o in listOfObjects
+                select o.InsantceNumber;
+
+            Console.WriteLine("Uruchamianie pętli foreach");
+            foreach (var number in result)
+            {
+                Console.WriteLine($"Zapis nr {number}");
+            }
         }
+
+
     }
 }
