987ec37f14bbaf5d083d988db5cfeddca4c21a11
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-11 18:14:22 +0200

    Dodaj pliki projektów.

---
+++ LINQtesting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQtesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
